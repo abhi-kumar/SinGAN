c95c6f9d34c6b88e2f4a95df44898d7377c9a537
Abhishek Kumar Annamraju <abhishek4273@gmail.com>
2022-02-08 13:35:04 +0530

    Updated to Single attention layer

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -10,7 +10,7 @@ class ConvBlock(nn.Sequential):
         super(ConvBlock,self).__init__()
         self.add_module('conv',nn.Conv2d(in_channel ,out_channel,kernel_size=ker_size,stride=stride,padding=padd)),
         self.add_module('norm',nn.BatchNorm2d(out_channel)),
-        self.add_module('LeakyRelu',nn.LeakyReLU(0.2, inplace=False))
+        self.add_module('LeakyRelu',nn.LeakyReLU(0.2, inplace=True))
         
 
 
@@ -32,9 +32,11 @@ class WDiscriminator(nn.Module):
         for i in range(opt.num_layer-2):
             N = int(opt.nfc/pow(2,(i+1)))
             block = ConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.ker_size,opt.padd_size,1)
-            #attn = AxialAttention(dim=max(N,opt.min_nfc), dim_index=1, dim_heads=8, heads=1, num_dimensions=2, sum_axial_out = True)
             self.body.add_module('block%d'%(i+1),block)
-            #self.body.add_module('attn%d'%(i+1),attn)
+            
+            if( i+1 == 3):
+                attn = AxialAttention(dim=max(N,opt.min_nfc), dim_index=1, dim_heads=32, heads=1, num_dimensions=2, sum_axial_out = True)
+                self.body.add_module('attn%d'%(i+1),attn)
         self.tail = nn.Conv2d(max(N,opt.min_nfc),1,kernel_size=opt.ker_size,stride=1,padding=opt.padd_size)
 
     def forward(self,x):
@@ -55,8 +57,9 @@ class GeneratorConcatSkip2CleanAdd(nn.Module):
             N = int(opt.nfc/pow(2,(i+1)))
             block = ConvBlock(max(2*N,opt.min_nfc),max(N,opt.min_nfc),opt.ker_size,opt.padd_size,1)
             self.body.add_module('block%d'%(i+1),block)
-            attn = AxialAttention(dim=max(N,opt.min_nfc), dim_index=1, dim_heads=8, heads=1, num_dimensions=2, sum_axial_out=True)
-            self.body.add_module('attn%d'%(i+1),attn)
+            if( i+1 == 3):
+                attn = AxialAttention(dim=max(N,opt.min_nfc), dim_index=1, dim_heads=32, heads=1, num_dimensions=2, sum_axial_out = True)
+                self.body.add_module('attn%d'%(i+1),attn)
         self.tail = nn.Sequential(
             nn.Conv2d(max(N,opt.min_nfc),opt.nc_im,kernel_size=opt.ker_size,stride =1,padding=opt.padd_size),
             nn.Tanh()
